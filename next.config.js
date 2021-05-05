5746296bedd8b52ebf505d964736bd4ac08c8515
Denis Zaratan Pasin <zaratan@hey.com>
2021-05-05 13:27:54 +0900

    DEBUG env #3

--- next.config.js
+++ next.config.js
@@ -8,5 +8,7 @@ module.exports = {
       'http://localhost:3000',
   },
 };
-process.env.AUTH0_BASE_URL ||=
-  `https://${process.env.VERCEL_URL}` || 'http://localhost:3000';
+process.env.AUTH0_BASE_URL =
+  process.env.AUTH0_BASE_URL ||
+  `https://${process.env.VERCEL_URL}` ||
+  'http://localhost:3000';
