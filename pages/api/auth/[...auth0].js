6491088a8baf289fa77b292d4a85b8632ff2bf1c
Denis Zaratan Pasin <zaratan@hey.com>
2021-05-05 13:58:11 +0900

    DEBUG env #9

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,11 +1,5 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-// eslint-disable-next-line dot-notation
-process.env['AUTH0_BASE_URL'] =
-  process.env.AUTH0_BASE_URL ||
-  `https://${process.env.VERCEL_URL}` ||
-  'http://localhost:3000';
-
-console.log({ env: process.env });
+console.log({ env: process.env, base_auth: process.env.AUTH0_BASE_URL });
 
 export default handleAuth();
