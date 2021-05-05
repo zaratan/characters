9ec163cc9839e2f755d2a920401ad42ace5a24fa
Denis Zaratan Pasin <zaratan@hey.com>
2021-05-05 13:33:53 +0900

    DEBUG env #5

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,10 +1,5 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-process.env.AUTH0_BASE_URL =
-  process.env.AUTH0_BASE_URL ||
-  `https://${process.env.VERCEL_URL}` ||
-  'http://localhost:3000';
-
 console.log({ env: process.env });
 
 export default handleAuth();
